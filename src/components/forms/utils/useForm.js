09ed9f455c43f83e63d441916b37796632bc9f21
Lovro Boric <lovro.boric@gmail.com>
2023-02-05 01:48:58 +0100

    refactor use form

--- src/components/forms/utils/useForm.js
+++ src/components/forms/utils/useForm.js
@@ -2,17 +2,18 @@ import { useState, useContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../../Context/GlobalContext";
+import {
+    baseUrl,
+    header,
+    navigate,
+
+} from "./reqData";
 
 export const useForm = (initialValues, mode, element) => {
     const [values, setValues] = useState(initialValues);
     const { user, setUser, games, setGames } = useContext(GlobalContext);
-    const baseUrl = process.env.REACT_APP_API_URL;
-
     const navigate = useNavigate();
-    const storedToken = localStorage.getItem("token");
-    const header = {
-        headers: { Authorization: `Bearer ${storedToken}` },
-    };
+
 
     const handleChange = (e) => {
         setValues((values) => {
@@ -32,7 +33,6 @@ export const useForm = (initialValues, mode, element) => {
                     localStorage.setItem("token", response.data.authToken);
                     localStorage.setItem("user", JSON.stringify(response.data.user));
                     setUser(response.data.user);
-                    // alert("Welcome back " + response.data.user.role + "!")
                     navigate("/games");
                 };
                 break;
