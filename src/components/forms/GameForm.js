ad77dd85f6cb0d80003bd65fcff3061bcbbbb4e0
Lovro Boric <lovro.boric@gmail.com>
2023-02-05 12:59:19 +0100

    added dummy data to autofill form for testing

--- src/components/forms/GameForm.js
+++ src/components/forms/GameForm.js
@@ -3,13 +3,46 @@ import { StyledForm } from "../styled/StyledForm";
 import { useForm } from "./utils/useForm";
 import { StyledButton } from "../styled/StyledButton";
 
+// array of animals, writtenin plural with first capital letter
+const animals = [
+  "Cats",
+  "Dogs",
+  "Elephants",
+  "Giraffes",
+  "Horses",
+  "Lions",
+  "Monkeys",
+  "Pigs",
+  "Rabbits",
+  "Sheep",]
+
+// array of cities
+
+const cities = [
+  "Berlin",
+  "Hamburg",
+  "Munich",
+  "Cologne",
+  "Frankfurt",
+  "Stuttgart",
+  "Dusseldorf",
+  "Dortmund",]
+
+const getTeamName = () => {
+  const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+  const randomCity = cities[Math.floor(Math.random() * cities.length)];
+
+  return `${randomCity} ${randomAnimal}`;
+};
+
+
 const backup = {
   league: "German National League",
-  round: "",
-  opponent: "",
-  venue: "",
-  startTime: "",
-  numberOfPlayers: "",
+  round: "1",
+  opponent: getTeamName(),
+  venue: "hall",
+  startTime: "05/05/2021 12:00",
+  numberOfPlayers: "12",
 };
 
 const GameForm = ({ mode, game, togglePageMode }) => {
@@ -46,6 +79,7 @@ const GameForm = ({ mode, game, togglePageMode }) => {
       return true
     } else {
       handleSubmit(e);
+      togglePageMode();
       return false
     }
   };
